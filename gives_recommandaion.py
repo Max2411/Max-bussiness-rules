ff4c9079ce3004572a3101e7d25a9d8ae6e6acc1
Max2411 <maxvankemenade@gmail.com>
2020-03-20 21:57:18 +0100

    update

--- gives_recommandaion.py
+++ gives_recommandaion.py
@@ -3,24 +3,6 @@ from connection import psycopg_connect
 
 cur, con=psycopg_connect()
 
-def test():
-    cur.execute("""select l.profid, l.prodid, pr.segment, r.name, r.category, r.subcategory, r.subsubcategory, r.targetaudience
-        from profiles_previously_viewed as L inner join profiles as Pr on l.profid = pr.id
-        inner join products as r on l.prodid = r.id
-        where segment = 'buyer' or segment = 'BUYER'
-        order by profid desc,
-        prodid desc
-        limit 100
-        """)
-
-    list = []
-    table = cur.fetchall()
-    for row in table:
-            subsubcategory = row[7]
-            list.append(subsubcategory)
-    print(list)
-#test()
-
 def recommend(profid):
     'Gives the recommendation'
     cur.execute("select * from fav_category")
